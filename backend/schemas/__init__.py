fbe73aa00c043fbc40c86b0cd4361e17eb72d95d
joao-motta <125322995+joao-motta@users.noreply.github.com>
2023-09-22 22:40:36 -0300

    Delete backend directory

--- backend/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.documento import DocumentoSchema, DocumentoBuscaSchema, DocumentoViewSchema, \
-                            ListagemDocumentosSchema, DocumentoDelSchema, apresenta_documentos, \
-                            apresenta_documento
-from schemas.error import ErrorSchema
