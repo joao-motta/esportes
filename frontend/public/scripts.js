a8197844c1bc6c12e62d6eb700b3ba741085e0a5
joao-motta <125322995+joao-motta@users.noreply.github.com>
2025-03-15 20:08:00 +0000

    commit

--- frontend/public/scripts.js
+++ frontend/public/scripts.js
@@ -1,3 +1,7 @@
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';  // Arquivo gerado pelo Amplify CLI
+Amplify.configure(awsconfig);
+
 document.addEventListener("DOMContentLoaded", function () {
     const salaSelect = document.getElementById("sala");
     const irParaSelecaoButton = document.getElementById("irParaSelecao");
